906fb801e5fa26cc33a0eac03ff3ba8ec3299b1f
Eder Martinez Lap UM Interfaces <edermtz.04@gmail.com>
2016-05-16 11:29:29 -0500

    original source code without task

---
+++ Assets/MTCClient/IClientEvents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MTConnectSharp
+{
+	[ComVisible(true)]
+	[InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+	public interface IClientEvents
+	{
+		void ProbeCompleted(object sender, EventArgs e);
+		void DataItemChanged(object sender, DataItemChangedEventArgs e);
+		void DataItemsChanged(object sender, EventArgs e);
+	}
+}
--- Assets/Source/TestClient.cs
+++ Assets/Source/TestClient.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using MTConnectSharp;
 
 public class TestClient : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		var client = new MTConnectClient ();
 	}
 	
 	// Update is called once per frame
